4bc1991f424036f4c390f0049bb51823f1349665
Grace Liang <gswliang@gmail.com>
2022-06-10 23:53:37 +0800

    implement UI display on quote card

--- src/App.jsx
+++ src/App.jsx
@@ -13,30 +13,24 @@ const App = () => {
   useEffect(() => {
     const subscribe = { op: "subscribe", args: ["orderBook:BTCPFC_0"] };
 
-    setOrders(data.data);
+    // setOrders(data.data);
 
-    // webSocket.onopen = () => {
-    //   webSocket.send(JSON.stringify(subscribe));
-    //   console.log("Connecting open!");
-    // };
-    // webSocket.onmessage = (event) => {
-    //   const result = JSON.parse(event.data);
-    //   setOrders(result.data);
-    // };
-
-    // webSocket.onclose = () => {
-    //   console.log("Connection Closed");
-    // };
-    // webSocket.onerror = (err) => {
-    //   console.log("Connection error: ", err);
-    // };
-  }, [orders]);
-
-  useEffect(() => {
-    return () => {
-      webSocket.close();
+    webSocket.onopen = () => {
+      webSocket.send(JSON.stringify(subscribe));
+      console.log("Connecting open!");
     };
-  }, []);
+    webSocket.onmessage = (event) => {
+      const result = JSON.parse(event.data);
+      setOrders(result.data);
+    };
+
+    webSocket.onclose = () => {
+      console.log("Connection Closed");
+    };
+    webSocket.onerror = (err) => {
+      console.log("Connection error: ", err);
+    };
+  }, [orders]);
 
   return (
     <div className="app">
--- src/Components/Quote/Quote.jsx
+++ src/Components/Quote/Quote.jsx
@@ -6,19 +6,34 @@ import "./quote.css";
 const Quote = ({ quote, type }) => {
   const SELL_COLOR = "#00b15d";
   const BUY_COLOR = "#FF5B5A";
-  const textColor = type === "sell" ? SELL_COLOR : BUY_COLOR;
+  const ACCUMULATIVE_SELL = "rgba(16, 186, 104, 0.12)";
+  const ACCUMULATIVE_BUY = "rgba(255, 90, 90, 0.12)";
+  const isSellQuote = type === "sell";
+
+  const textColor = isSellQuote ? SELL_COLOR : BUY_COLOR;
   const [total, setTotal] = useState(null);
 
   const quoteData = quote.map((q, index) => {
     const price = numberFormat(q.price);
     const size = numberFormat(q.size);
     const currentTotal = numberFormat(`${q.total}`);
+    const accumulativeBarPercentage = (q.total / total) * 100;
+    const backgroundColor = isSellQuote ? ACCUMULATIVE_SELL : ACCUMULATIVE_BUY;
 
     return (
       <div className="table" key={index}>
         <div style={{ color: textColor }}>{price}</div>
         <div>{size}</div>
-        <div className="accumulative">{currentTotal}</div>
+        <div className="accumulative">
+          <div> {currentTotal}</div>
+          <div
+            className="accumulative-shadow"
+            style={{
+              backgroundColor: backgroundColor,
+              width: `${accumulativeBarPercentage}%`,
+            }}
+          ></div>
+        </div>
       </div>
     );
   });
@@ -27,7 +42,7 @@ const Quote = ({ quote, type }) => {
     if (!quote.length) {
       return;
     }
-    const index = type === "sell" ? 0 : quote.length - 1;
+    const index = isSellQuote ? 0 : quote.length - 1;
     const total = quote[index]?.total;
     setTotal(total);
   }, [quote]);
